c592302f8579491ce8f47d04c7bcc295181e4d1a
Riggy <154659854+Rigyaju@users.noreply.github.com>
2024-10-27 22:38:17 -0700

    Edited JS function to hide hamburger menu when not in use

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 var navlinks = document.getElementById("navLinks");
 
 function displayMenu(){
+    navlinks.style.display = "block"
     navLinks.style.right = "0";
 }
 
 function closeMenu(){
     navLinks.style.right = "-200px";
+
+    navlinks.ontransitionend = function(){
+        navlinks.style.display = "none";
+    }
+
 }
